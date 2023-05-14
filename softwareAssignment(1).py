e48ba29d2f442199b8c4dccfdda5f8d367a61e7d
Greg Shook <84742775+gregshook@users.noreply.github.com>
2023-05-14 12:36:36 +0200

    Update softwareAssignment(1).py

--- softwareAssignment(1).py
+++ softwareAssignment(1).py
@@ -1,8 +1,3 @@
-'''
-Introduction to Python Programming (aka Programmierkurs I, aka Python I)
-Software Assignment
-'''
-
 import sys
 import xml.etree.ElementTree as ET
 import string as string
@@ -15,20 +10,7 @@ from collections import Counter
 class SearchEngine:
 
     def __init__(self, collectionName, create):
-        '''
-        Initialize the search engine, i.e. create or read in index. If
-        create=True, the search index should be created and written to
-        files. If create=False, the search index should be read from
-        the files. The collectionName points to the filename of the
-        document collection (without the .xml at the end). Hence, you
-        can read the documents from <collectionName>.xml, and should
-        write / read the idf index to / from <collectionName>.idf, and
-        the tf index to / from <collectionName>.tf respectively. All
-        of these files must reside in the same folder as THIS file. If
-        your program does not adhere to this "interface
-        specification", we will subtract some points as it will be
-        impossible for us to test your program automatically!
-        '''
+    
         self.collectionName = collectionName
 
         if create:
@@ -195,13 +177,7 @@ class SearchEngine:
         Input to this function: List of query terms
 
         Returns the 10 highest ranked documents together with their
-        tf.idf-sum scores, sorted score. For instance,
-
-        [('NYT_ENG_19950101.0001', 0.07237004260325626),
-         ('NYT_ENG_19950101.0022', 0.013039249597972629), ...]
-
-        May be less than 10 documents if there aren't as many documents
-        that contain the terms.
+        tf.idf-sum scores, sorted score.
         '''
 
         # Process query text
@@ -251,13 +227,7 @@ class SearchEngine:
         return out
 
 if __name__ == '__main__':
-    '''
-    write your code here:
-    * load index / start search engine
-    * start the loop asking for query terms
-    * program should quit if users enters no term and simply hits enter
-    '''
-    # Example for how we might test your program:
-    # Should also work with nyt199501 !
+   
+    # Example to test program
     searchEngine = SearchEngine("nyt199501", create=False)
     searchEngine.executeQueryConsole()
